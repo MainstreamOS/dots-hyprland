c202752aa9c01b97c2f0887f4ea7c0d9e22a3446
gregorytrentmartinjr <110754385+gregorytrentmartinjr@users.noreply.github.com>
2026-07-20 07:44:08 -0500

    keybinds: track Hyprland 0.56 scroll axis naming fix

--- dots/.config/hypr/hyprland/keybinds.lua
+++ dots/.config/hypr/hyprland/keybinds.lua
@@ -120,7 +120,7 @@ end
 for i = 1, 4 do
  local key = {"SUPER + SHIFT + mouse_", "SUPER + ALT + mouse_"}
  local keycombos = {key[1].."down", key[1].."up", key[2].."down", key[2].."up"}
- local prefix = {"r-","r+","-","+"}
+ local prefix = {"r+","r-","+","-"}
  hl.bind(keycombos[i], hl.dsp.window.move({workspace = prefix[i].."1"}) )
 end
 
@@ -193,7 +193,7 @@ for i = 1, 4 do
 end
 --#/# bind = SUPER + Scroll ↑/↓,, -- Focus left/right
 for i = 1, 4 do
- local key = {"SUPER + mouse_up", "SUPER + mouse_down"}
+ local key = {"SUPER + mouse_down", "SUPER + mouse_up"}
  local keycombos = {key[1], key[2], "CTRL + "..key[1], "CTRL + "..key[2]}
  local prefix = {"+","-","r+","r-"}
  hl.bind(keycombos[i], hl.dsp.focus({workspace = prefix[i].."1"}) )
